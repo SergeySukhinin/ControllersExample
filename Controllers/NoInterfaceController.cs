6357df9ce68c38df39233267c71b5b9a8aa2c779
Sergey Sukhinin <3723901@gmail.com>
2024-10-01 23:08:26 -0400

    sample controllers added

---
+++ Controllers/NoInterfaceController.cs
@@ -0,0 +1,28 @@
+﻿//using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    [Route("[controller]")]
+    public class NoInterfaceController
+    {
+        [HttpGet("/sayhello")]
+        [HttpGet("/")]
+        public String Index()
+        {
+            return "Hello from Index";
+        }
+
+        [HttpGet("/about")]
+        public String About()
+        {
+            return "Hello from About";
+        }
+
+        [HttpGet("/contact-us")]
+        public String ContactUs()
+        {
+            return "Hello from ContactUs";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using ControllersExample.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddTransient<NoInterfaceController>(); // for adding controllers as a service (without using IController interface), other words, for adding a custom controller to a service class, so it will participate in dependency injection (an instance of the class will be created
+builder.Services.AddControllers(); // instead of adding each controller
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();   // this can be used instead of defining multiple statements: app.UseRouting() and app.UseEndpoints(endpoints => {endpoints.Map(...)})
 app.Run();
