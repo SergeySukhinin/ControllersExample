88c8c96bb14c7d13cc196e7d4622b738bca011e6
Sergey Sukhinin <3723901@gmail.com>
2024-10-28 15:53:07 -0400

    Redirection examples added

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -21,8 +21,8 @@ namespace ControllersExample.Controllers
                 //return Content("id is not supplied");
                 return BadRequest("id is not supplied");    // status 400
             }
-            int id = Convert.ToInt16(Request.Query["id"]);
-            if ( id <= 0 || id >1000) {
+            int itemId = Convert.ToInt16(Request.Query["id"]);
+            if ( itemId <= 0 || itemId >1000) {
                 //Response.StatusCode = 400;
                 //return Content("id can not be less than 0 or greater than 1000");
                 //return new BadRequestResult();    //there is a shortcut:
@@ -36,7 +36,39 @@ namespace ControllersExample.Controllers
                 return Unauthorized("User must be authenticated");  // status 401
             }
 
-            return Json(new Product(Convert.ToUInt16(Request.Query["id"])));
+            //return Json(new Product(Convert.ToUInt16(Request.Query["id"])));
+
+            /***
+              response is 302 - Found (temporary redirect
+            //return new RedirectToActionResult("Books", "Store", new { }); //without passing any parameters
+            // shortcut for redirect:
+            //return RedirectToAction("Books", "Store", new { id = itemId });
+
+            // response is 301 - Moved Permanently
+
+            // BE CAREFUL WITH USING RedirectToActionPERMANENT!!! Your browser will go straight to the new controller without going through this code.
+            // In case you change your code here, you will get issues, and in this case you will need clear your browser's cache and IIS Express Cache
+
+            //return new RedirectToActionResult("Books", "Store", new { }, permanent: true); 
+            //shortcut for permanent redirect:
+            //return RedirectToActionPermanent("Books", "Store", new { id = itemId });
+
+            //return new RedirectToActionResult("Books", "Store", new Product(Convert.ToUInt16(Request.Query["id"])));
+            */
+
+            /***
+             * ANOTHER WAY TO REDIRECT USING A URL:
+             */
+            // response 302 - Found
+            return new LocalRedirectResult($"/store/books/{itemId}");
+            // the shortcut is:
+            //return new RedirectResult($"/store/books/{itemId}");
+            // or:
+            //return Redirect($"/store/books/{itemId}");
+
+            //response 301 - Moved Permanently
+            //return new RedirectResult($"/store/books/{itemId}", true);
+            //return RedirectPermanent($"/store/books/{itemId}");
         }
     }
 }
---
+++ Controllers/StoreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    public class StoreController : Controller
+    {
+        [HttpGet("store/books/{id?}")]
+        public IActionResult Books([FromRoute] int id)
+        {
+            //return View();
+            
+            return Content($"<h1>Book Store id={id}</h1>", "text/html");
+        }
+    }
+}
+//products?islogged=true&id=225
